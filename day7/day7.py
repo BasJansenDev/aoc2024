e6d8c6fec459bf8d7558953c5efad0a2a725b9e0
Bas Jansen <62569615+BasJansenDev@users.noreply.github.com>
2024-12-07 11:00:57 +0100

    Recursion for faster implementation

--- day7/day7.py
+++ day7/day7.py
@@ -1,35 +1,27 @@
-from itertools import product
+def calculate(result, expression, parts):
+    if parts == []:
+        return expression == result
+    else:
+        return calculate(result, expression + parts[0], parts[1:]) \
+            or calculate(result, expression * parts[0], parts[1:]) \
+            or (part2 and calculate(result, int(str(expression) + str(parts[0])), parts[1:]))
 
-def main(part1):
+def main():
     input = read_input()
     success = 0
     for equation in input:
         result, parts = equation.split(':')
         result = int(result)
         parts = list(map(int,parts.strip().split(' ')))
-        operators = []
-        if(part1):
-            operators = list(product(['+', '*'], repeat=len(parts)-1))
-        else:
-            operators = list(product(['+', '*', '||'], repeat=len(parts)-1))
-        for ops in operators:
-            expression = parts[0]
-            for i, op in enumerate(ops):
-                if op == '+':
-                    expression += parts[i + 1]
-                elif op == '*':
-                    expression *= parts[i + 1]
-                else:
-                    expression = int(str(expression) + str(parts[i+1]))
-            if(expression == result):
-                success += result
-                break
+        if(calculate(result, parts[0], parts[1:])):
+            success += result
     return success
-
 
 def read_input():
     f = open('input')
     return f.read().split('\n')
 
-print(main(True))
-print(main(False))
+part2 = False
+print(main())
+part2 = True
+print(main())
