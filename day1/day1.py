3c4d5bd8b556328b010c5dbce982465030f73936
Bas Jansen <62569615+BasJansenDev@users.noreply.github.com>
2024-12-08 11:34:07 +0100

    Day 1

--- day1/day1.py
+++ day1/day1.py
@@ -1,28 +1,12 @@
 def main(part1):
-    first,second = split_list(input_as_list())
+    first,second = map(list, zip(*[i.split('   ') for i in open('input').read().split('\n')]))
     first.sort()
     second.sort()
     sum = 0
     for i in range(len(first)) :
-        if(part1):
-            sum += abs(int(first[i]) - int(second[i]))
-        else:
-            sum += int(second.count(first[i]))*int(first[i])
+        if(part1): sum += abs(int(first[i]) - int(second[i]))
+        else:      sum += int(second.count(first[i]))*int(first[i])
     return sum
-
-
-def split_list(list):
-    a = []
-    b = []
-    for i in list:
-        c,d = i.split('   ')
-        a += [c]
-        b += [d]
-    return a,b
-
-def input_as_list():
-    f = open('input')
-    return list(f.read().split('\n'))
 
 print(main(True))
 print(main(False))
