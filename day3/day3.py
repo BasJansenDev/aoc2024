1e7a753121f31d28d8b784c61db4b8e50fc69dcd
Bas Jansen <62569615+BasJansenDev@users.noreply.github.com>
2024-12-08 11:40:50 +0100

    Day 3

--- day3/day3.py
+++ day3/day3.py
@@ -6,10 +6,9 @@ dont_pattern = r"don't\(\)"
 pattern = f"({mul_pattern})|({do_pattern})|({dont_pattern})"
 
 def main(part1):
-    text = read_input()
     sum = 0
     do = True
-    for match in re.finditer(pattern, text):
+    for match in re.finditer(pattern, open('input').read()):
         if match.group(1) and (do or part1):
             sum += int(match.group(2)) * int(match.group(3))
         elif match.group(4):
@@ -17,10 +16,6 @@ def main(part1):
         elif match.group(5):
             do = False
     return sum
-
-def read_input():
-    f = open('input')
-    return f.read()
-
+    
 print(main(True))
 print(main(False))
