fe1c507781bb3c09faa154145d41df5e3feed53e
Bas Jansen <62569615+BasJansenDev@users.noreply.github.com>
2024-12-08 11:38:41 +0100

    Day 2

--- day2/day2.py
+++ day2/day2.py
@@ -1,26 +1,15 @@
 def main(part1):
-    input = input_as_list()
-    total = 0
+    input = [[int(x) for x in i.split(' ')] for i in open('input').read().split('\n')]
     if(part1):
-        total = sum(check(lst) for lst in input)
-    else:
-        for lst in input:
-            for i in range(len(lst)):
-                sublist = lst[:i] + lst[i + 1:]
-                if (check(sublist)):
-                    total += 1
-                    break
-    return total
+        return sum(check(lst) for lst in input)
+    return sum(1 for lst in input if any(check(lst[:i] + lst[i + 1:]) for i in range(len(lst))))
+
 
 def check(sublist):
     ascending = all(earlier > later for earlier, later in zip(sublist, sublist[1:]))
     descending = all(earlier < later for earlier, later in zip(sublist, sublist[1:]))
     diff = all(abs(earlier - later) <= 3 for earlier, later in zip(sublist, sublist[1:]))
     return (ascending or descending) and diff
-
-def input_as_list():
-    f = open('input')
-    return [[int(x) for x in i.split(' ')] for i in f.read().split('\n')]
 
 print(main(True))
 print(main(False))
